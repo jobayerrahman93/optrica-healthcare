3d8c1c873fede24d5db86056856de4a490535322
jobayercse71 <jobayercse71.com>
2021-10-19 23:33:47 +0600

    section update

--- src/component/Pages/Commited/Commited.js
+++ src/component/Pages/Commited/Commited.js
@@ -3,9 +3,9 @@ import './Commited.css';
 
 const Commited = () => {
     return (
-        <div className="commited text-center py-5 ">
+        <div className="commited text-center my-5 ">
             <h5>TIME CAN'T BE RESISTED, BUT AGING CAN</h5>
-            <h1 className="mb-3">Commited To Trusted Health Care Delmont</h1>
+            <h1 className="mb-3">Commited To Trusted Health Care</h1>
             <h4 className="mb-5">Get Your Quote or Call: <span>(0080) 123-453-789)</span></h4>
             <div className="mb-5">
             <button className="view me-5 mb-2">Appointment</button>
--- src/component/Pages/MedicalResearch/MedicalResearch.js
+++ src/component/Pages/MedicalResearch/MedicalResearch.js
@@ -6,25 +6,25 @@ const MedicalResearch = () => {
         <div className="medical">
             <div className="container">
                 <div className="row py-5 ">
-                    <div className="col-md-8" data-aos="fade-right" data-aos-duration="2000" data-aos-delay="300">
+                    <div className="col-md-8 " data-aos="fade-right" data-aos-duration="2000" data-aos-delay="300">
                         <h1>We’re Setting the Standards in Research & Clinical Care</h1>
                         <p className="des">We provide the most full medical services, so every person could have the opportunity to receive qualitative medical help. Our Clinic has grown to provide a world class facility for the treatment of tooth loss, dental cosmetics and bore advanced restorative dentistry. We are among the most qualified implant providers in the USA with over 35 years of quality training and experience.</p>
-                        <div className="row">
-                            <h3>Optrica Special Features</h3>
+                        <div className="row m-0">
+                            <h3 className="pe-0">Optrica Special Features</h3>
 
-                            <div className="col-md-6">
+                            <div className="col-md-6 pe-0">
                                 <p>Adult Trauma Center</p>
                                 <p>Birthing and Lactation Classes</p>
                                 <p>ental and Oral Surgery</p>
 
                             </div>
-                            <div className="col-md-6">
+                            <div className="col-md-6 pe-0">
                                 <p>Children's Trauma Center</p>
                                 <p>Heart and Vascular Institute</p>
                                 <p>Children's Trauma Center</p>
                             </div>
 
-                            <div className="w-100">
+                            <div className="w-100 pe-0">
                             <button className="view me-2 mb-2">VIEW MORE</button>
                             <button className="contact me-5">CONTACT US</button>
                             </div>
